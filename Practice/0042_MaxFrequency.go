02d84d4a367c6a2d48e688842a9903d8739d77f8
harshit-273 <kalavadiaharshit@gmail.com>
2022-08-20 17:44:26 +0530

    first charcter with max frequency added

--- Practice/0042_MaxFrequency.go
+++ Practice/0042_MaxFrequency.go
@@ -15,7 +15,7 @@ func MaxFrequency(str string) (freq int, ch rune) {
 	freq = 0
 	ch = ' '
 	for index, value := range freqMap {
-		if value > freq {
+		if value > freq { // if we use ">=" instead of ">" then last character with most frequency will be the answer
 			freq = value
 			ch = index
 		}
@@ -24,6 +24,6 @@ func MaxFrequency(str string) (freq int, ch rune) {
 }
 
 func main() {
-	freq, ch := MaxFrequency("harshita")
+	freq, ch := MaxFrequency("aaabbccc")
 	fmt.Printf("%c is present %d times in the string", ch, freq)
 }
