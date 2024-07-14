67f54dba66e5e0a4df6c95378efc8e9f98d6c705
harshit-273 <kalavadiaharshit@gmail.com>
2024-07-14 22:08:56 +0530

    reverse array using recursion

---
+++ StriverSheet/Basics/Recursion/ReverseArray.go
@@ -0,0 +1,25 @@
+/*
+You are given an array. The task is to reverse the array and print it. 
+
+We would be swapping 1st and last indices till the mid.
+*/
+
+package main
+
+import "fmt"
+
+func revArr(arr []int, nth int, lastNth int) ([]int) {
+	var temp int
+	if nth < lastNth {
+		temp = arr[nth]
+		arr[nth] = arr[lastNth]
+		arr[lastNth] = temp
+		revArr(arr, nth+1, lastNth-1)
+	}
+	return arr
+}
+
+func main() {
+	var ar []int = []int{0, 1, 2, 3, 4, 5}
+	fmt.Printf("%v", revArr(ar, 0, len(ar)-1))
+}
